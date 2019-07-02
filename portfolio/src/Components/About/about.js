44c977352a227527894ca502712bbbf269575480
Saheed Oladosu <oladosu777@gmail.com>
2019-07-02 14:03:01 +0100

    new about

---
+++ portfolio/src/Components/About/about.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './about.css';
+
+class About extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>About me</h2>
+                <p>Senior IT Officer turned Front-end Developer with more than 2 years of experience designing, developing, and managing complex sites and internal frameworks. 
+                    Specializes in ReactJS and responsive design. 
+                    I'm the Creator OSS for Newbies, an Open Source Software project that helps Newbies make their first contribution on Github and also support them in their Software development journey.
+                    I'm also the Coordinator of a React North-central Meetup with over 220 members in Nigeria. 
+                    Members come up with different topics on ReactJS and relevant software development tricks to help other member. 
+                    When I'm not coding you can find me writing a blog post, listening to musics, watching my favorite football team @manunited, or playing football.
+                </p>
+            </div>
+        )
+
+    }
+}
+
+export default About;
