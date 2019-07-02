b22541a507d9fd3fb5ea91ecdb071558f1f19385
Saheed Oladosu <oladosu777@gmail.com>
2019-07-02 14:03:26 +0100

    new skills

---
+++ portfolio/src/Components/Skills/skills.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './skills.css';
+
+class Skills extends React.Component{
+    render(){
+        return(
+            <div>
+               <h1>I want to work with:</h1>
+               <h2>JavaScript</h2> 
+               <h2>ReactJS</h2> 
+               <h2>NodeJS</h2> 
+               <h2>ExpressJS</h2> 
+               <h2>PostgreSQL</h2> 
+               <h2>MongoDB</h2> 
+               <h2>MySQL</h2> 
+               <h2>CSS3</h2>
+               <h2>HTML5</h2>  
+               <h2>GraphQL</h2>
+               <h2>Test Driven Development(TDD)</h2>  
+               <h2>Typscript</h2> 
+               <h2>Git</h2> 
+               <p>...and I can easily pickup new technologies and tools.</p>
+            </div>
+        )
+    }
+}
+
+export default Skills;
